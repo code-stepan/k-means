6a501a227fd2a3ef80caddb76f10f79b2c3804d9
code-stepan <108977209+code-stepan@users.noreply.github.com>
2025-05-18 19:39:57 +0300

    update

--- k-means.py
+++ k-means.py
@@ -1,1 +1,17 @@
-print("k-means")
+import random
+import math
+import matplotlib as plt
+
+
+class Kmeans:
+    def __init__(self, points, k, max_iterations=100):
+        self.points = points
+        self.k = k
+        self.max_iterations = max_iterations
+        self.centroids = random.sample(self.points, self.k)
+        self.assignments = []
+        self.iterations = 0
+        self.first_centroids = self.centroids[:]\
+
+    def run(self):
+        pass
